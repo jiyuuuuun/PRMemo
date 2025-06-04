b8b8404f51057b7fe2cbebaed5ffa1d597b538f6
jiyuuuuun <hwangjy001@naver.com>
2025-06-04 18:18:24 +0900

    first commit

---
+++ src/main/java/io/github/jiyuuuuun/PRMemo/PrMemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.jiyuuuuun.PRMemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jiyuuuuun/PRMemo/PrMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jiyuuuuun.PRMemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
