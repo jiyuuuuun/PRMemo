fb2ba09cc9e013d5e9d8478470221ebc78148a11
jiyuuuuun <hwangjy001@naver.com>
2025-06-10 19:16:56 +0900

    [ back ] 1 : 시큐리티 설정 수정

--- backend/src/main/java/io/github/jiyuuuuun/PRMemo/security/SecurityConfig.java
+++ backend/src/main/java/io/github/jiyuuuuun/PRMemo/security/SecurityConfig.java
@@ -3,16 +3,22 @@ package io.github.jiyuuuuun.PRMemo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class SecurityConfig {
 
     private final OAuth2SuccessHandler successHandler;
+    private final JwtAuthenticationFilter jwtFilter;
 
-    public SecurityConfig(OAuth2SuccessHandler successHandler) {
+    public SecurityConfig(OAuth2SuccessHandler successHandler, JwtAuthenticationFilter jwtFilter) {
         this.successHandler = successHandler;
+        this.jwtFilter = jwtFilter;
     }
 
     @Bean
@@ -23,10 +29,28 @@ public class SecurityConfig {
                         .requestMatchers("/", "/oauth2/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .oauth2Login(oauth2 -> oauth2
                         .successHandler(successHandler)
-                );
+                )
+                .formLogin(form -> form.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(httpBasic -> httpBasic.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
