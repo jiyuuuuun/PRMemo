8f511d385933eca74e93eb71d619c200289bbd6b
jiyuuuuun <hwangjy001@naver.com>
2025-06-10 19:16:44 +0900

    [ back ] 1 : validateToken 구현

--- backend/src/main/java/io/github/jiyuuuuun/PRMemo/security/util/JwtUtil.java
+++ backend/src/main/java/io/github/jiyuuuuun/PRMemo/security/util/JwtUtil.java
@@ -1,8 +1,7 @@
 package io.github.jiyuuuuun.PRMemo.security.util;
 
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -30,5 +29,34 @@ public class JwtUtil {
                 .setExpiration(expiry)
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(secret) // 서명 검증
+                    .parseClaimsJws(token); // 예외 없으면 유효함
+            return true;
+        } catch (SignatureException e) {
+            System.out.println("❌ JWT 서명 오류: " + e.getMessage());
+        } catch (MalformedJwtException e) {
+            System.out.println("❌ 잘못된 JWT 형식: " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            System.out.println("❌ 만료된 JWT: " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            System.out.println("❌ 지원되지 않는 JWT: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("❌ 비어 있는 JWT claims: " + e.getMessage());
+        }
+
+        return false;
+    }
+
+    public String getSubject(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret) // 서명 키 설정
+                .parseClaimsJws(token) // 토큰 파싱 및 검증
+                .getBody()             // 클레임 접근
+                .getSubject();         // subject(claims.sub) 추출
     }
 }
